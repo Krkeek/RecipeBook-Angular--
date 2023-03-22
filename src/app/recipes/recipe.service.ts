b08b78b6429789d4f74cede1457f1a382eaf1ab4
Ahmad Hijazi <ahmadhijazi_me@hotmail.com>
2023-03-22 21:18:14 +0100

    update

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,17 +11,24 @@ export class RecipeService {
     new Recipe(
       'Tasty Schnitzel',
       'A super-tasty Schnitzel - just awesome!',
-      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      'https://cdn.gutekueche.de/upload/rezept/371/wiener-schnitzel.jpg',
       [
         new Ingredient('Meat', 1),
         new Ingredient('French Fries', 20)
       ]),
     new Recipe('Big Fat Burger',
       'What else you need to say?',
-      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      'https://img.freepik.com/premium-photo/single-fresh-burger_693588-34.jpg?w=740',
       [
-        new Ingredient('Buns', 2),
+        new Ingredient('Cheese', 2),
         new Ingredient('Meat', 1)
+      ]),
+      new Recipe('Salami Pizza',
+      'Best Pizza Ever!',
+      'https://previews.123rf.com/images/boarding1now/boarding1now1212/boarding1now121200021/16690745-frische-salami-pizza-isoliert-auf-wei%C3%9Fem-hintergrund.jpg',
+      [
+        new Ingredient('Salami', 2),
+        new Ingredient('Tomato Sauce', 1)
       ])
   ];
 
