2849b32be73c0df8842fe38ba12c14a611b9c9b3
Ahmad Hijazi <ahmadhijazi_me@hotmail.com>
2023-04-14 13:11:00 +0200

    recipeList

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -29,7 +29,70 @@ export class RecipeService {
       [
         new Ingredient('Salami', 2),
         new Ingredient('Tomato Sauce', 1)
-      ])
+      ]), new Recipe(
+        'Tasty Schnitzel',
+        'A super-tasty Schnitzel - just awesome!',
+        'https://cdn.gutekueche.de/upload/rezept/371/wiener-schnitzel.jpg',
+        [
+          new Ingredient('Meat', 1),
+          new Ingredient('French Fries', 20)
+        ]),
+      new Recipe('Big Fat Burger',
+        'What else you need to say?',
+        'https://img.freepik.com/premium-photo/single-fresh-burger_693588-34.jpg?w=740',
+        [
+          new Ingredient('Cheese', 2),
+          new Ingredient('Meat', 1)
+        ]),
+        new Recipe('Salami Pizza',
+        'Best Pizza Ever!',
+        'https://previews.123rf.com/images/boarding1now/boarding1now1212/boarding1now121200021/16690745-frische-salami-pizza-isoliert-auf-wei%C3%9Fem-hintergrund.jpg',
+        [
+          new Ingredient('Salami', 2),
+          new Ingredient('Tomato Sauce', 1)
+        ]), new Recipe(
+          'Tasty Schnitzel',
+          'A super-tasty Schnitzel - just awesome!',
+          'https://cdn.gutekueche.de/upload/rezept/371/wiener-schnitzel.jpg',
+          [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20)
+          ]),
+        new Recipe('Big Fat Burger',
+          'What else you need to say?',
+          'https://img.freepik.com/premium-photo/single-fresh-burger_693588-34.jpg?w=740',
+          [
+            new Ingredient('Cheese', 2),
+            new Ingredient('Meat', 1)
+          ]),
+          new Recipe('Salami Pizza',
+          'Best Pizza Ever!',
+          'https://previews.123rf.com/images/boarding1now/boarding1now1212/boarding1now121200021/16690745-frische-salami-pizza-isoliert-auf-wei%C3%9Fem-hintergrund.jpg',
+          [
+            new Ingredient('Salami', 2),
+            new Ingredient('Tomato Sauce', 1)
+          ]), new Recipe(
+            'Tasty Schnitzel',
+            'A super-tasty Schnitzel - just awesome!',
+            'https://cdn.gutekueche.de/upload/rezept/371/wiener-schnitzel.jpg',
+            [
+              new Ingredient('Meat', 1),
+              new Ingredient('French Fries', 20)
+            ]),
+          new Recipe('Big Fat Burger',
+            'What else you need to say?',
+            'https://img.freepik.com/premium-photo/single-fresh-burger_693588-34.jpg?w=740',
+            [
+              new Ingredient('Cheese', 2),
+              new Ingredient('Meat', 1)
+            ]),
+            new Recipe('Salami Pizza',
+            'Best Pizza Ever!',
+            'https://previews.123rf.com/images/boarding1now/boarding1now1212/boarding1now121200021/16690745-frische-salami-pizza-isoliert-auf-wei%C3%9Fem-hintergrund.jpg',
+            [
+              new Ingredient('Salami', 2),
+              new Ingredient('Tomato Sauce', 1)
+            ])
   ];
 
   constructor(private slService: ShoppingListService) {}
