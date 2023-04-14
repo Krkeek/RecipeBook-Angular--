e7bbc621745408383ef73a4f17d9196b400e0176
Ahmad Hijazi <ahmadhijazi_me@hotmail.com>
2023-04-14 11:26:47 +0200

    scrollButton

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
-
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.css']
+  styleUrls: ['./welcome-page.component.css'],
 })
 export class WelcomePageComponent {
 
+  constructor(private appService: AppService){}
+
   lookForRecipes(){
-   
+    this.appService.OnLooktoRecipes.emit(true);
   }
 
 }
