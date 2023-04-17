0e745e023cb96e3257650617a9328bdb74eb8a1f
Ahmad Hijazi <ahmadhijazi_me@hotmail.com>
2023-04-17 19:34:24 +0200

    database

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Recipe } from "../recipes/recipe.model";
+import { map, tap } from "rxjs";
+
+@Injectable()
+
+export class DataStorageService {
+    constructor(private http: HttpClient) {}
+
+        
+    addNewRecipe(recipe: Recipe){
+            return this.http.post('https://recipebook-krkeek-default-rtdb.europe-west1.firebasedatabase.app/recipes.json', recipe);
+    }
+
+    updateRecipe(index: number, recipe: Recipe){
+        
+    }
+
+    fetchRecipes(){
+        return this.http.get<Recipe[]>('https://recipebook-krkeek-default-rtdb.europe-west1.firebasedatabase.app/recipes.json');
+
+
+    }
+} 
+
+
