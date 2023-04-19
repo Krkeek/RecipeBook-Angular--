646ca7c4351d23cfe468c7b630e091816732d1ae
Ahmad Hijazi <ahmadhijazi_me@hotmail.com>
2023-04-19 19:41:34 +0200

    spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent implements OnInit {
 
 
   constructor(
+    
     ){}
   
   ngOnInit(): void {
---
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-loading-spinner',
+    template: '<div class="loadingio-spinner-dual-ball-q7ub2cutm7q"><div class="ldio-o02ecehbcsm"><div></div><div></div><div></div></div></div>',
+    styleUrls: ['./loading-spinner.component.css']
+})
+export class LoadingSpinnerComponent{
+
+}
